ace1c5b48b0e7dd8ea42081bbf7dda05057d8386
Oguntuberu Nathan <nateoguns@localhost.localdomain>
2018-10-28 22:41:17 +0100

    wrote skeleton routing handlers for /user routes

---
+++ controller/user.js
@@ -0,0 +1,29 @@
+/*  */
+const express = require('express');
+const router = express.Router();
+
+router.get('/free', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/add', (req, res) =>{
+
+});
+
+router.put('/free', (req, res) => {
+
+});
+
+router.delete('/remove', (req, res) => {
+
+});
+
+
+
+
+/*  Export Controller*/
+module.exports = router;
