ee056ab1d0a327b626767f281e5ebd8874f8a346
Oguntuberu Nathan <nateoguns@localhost.localdomain>
2018-10-28 22:40:30 +0100

    wrote skeleton routing handlers for /chat routes

---
+++ controller/chat.js
@@ -0,0 +1,24 @@
+/*  */
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id/message/all', (req, res) => {
+
+});
+
+router.get('/:id/message/unread', (req, res) => {
+
+});
+
+router.post('/message', (req, res) =>{
+
+});
+
+router.post('/add', (req, res) => {
+
+});
+
+
+
+/*  Export Controller*/
+module.exports = router;
