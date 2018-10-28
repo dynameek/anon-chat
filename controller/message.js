3968d4f58c5c6d84d9814282756b65eb63bdb345
Oguntuberu Nathan <nateoguns@localhost.localdomain>
2018-10-28 22:41:37 +0100

    wrote skeleton routing handlers for /message routes

---
+++ controller/message.js
@@ -0,0 +1,13 @@
+/*  */
+const express = require('express');
+const router = express.Router();
+
+router.put('/:id', (req, res) => {
+
+});
+
+
+
+
+/*  Export Controller*/
+module.exports = router;
