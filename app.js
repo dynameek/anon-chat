68d4471f280e9b4f1212f926ffb4d8d4413ab4a5
Oguntuberu Nathan <nateoguns@localhost.localdomain>
2018-10-28 22:39:43 +0100

    set up app.js: app's entry

---
+++ app.js
@@ -0,0 +1,43 @@
+/*  */
+const express = require('express');
+const bodyParse = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+/*  Import Database Config  */
+const dbConfig = require('database/db');
+
+/*  connect to database*/
+mongoose.connect(dbConfig.database);
+
+/*  Import Route Handlers   */
+const user = require('controller/user');
+const chat = require('controller/chat');
+const message = require('controller/message');
+
+/*  */
+const app = express();
+
+/*  */
+app.use(cors());
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended: true}));
+
+/*  Set page parser*/
+app.use(express.static(path.join(_dirname, 'public')));
+
+/*  Routing */
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.use('/user', user);
+app.use('/chat', chat);
+app.use('/message', message);
+
+/*  Serving the application */
+const port = 3000;
+app.listen(port, () => {
+    console.log('Listening on port: '+port);
+});
